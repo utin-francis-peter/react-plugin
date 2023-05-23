7a0c713694cc85353057c565c81fb471a90094fb
utin-francis-peter <u.francispeter@gmail.com>
2023-05-23 10:37:22 +0100

    feat: added more props to button component

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,10 +1,26 @@
-export const Button = ({
+export const ButtonNew = ({
   label,
   onClick,
   variant,
-  size,
   loading,
   fullWidth,
+  small,
+  disabled,
 }) => {
-  return <button className="p-4 bg-red-800 text-white rounded">{label}</button>;
+  const variantClasses = `${
+    variant === "secondary"
+      ? "bg-gray-600 hover:bg-gray-700"
+      : "bg-gray-900 hover:bg-black"
+  }`;
+  const smallClasses = `${small ? "text-sm py-2 px-3" : "text-base p-4"}`;
+  const fullWidthClasses = `${fullWidth ? "w-full" : "w-auto"}`;
+
+  return (
+    <button
+      className={`rounded text-white disabled:bg-gray-600 ${variantClasses} ${fullWidthClasses} ${smallClasses}`}
+      onClick={onClick}
+      disabled={disabled}>
+      {label}
+    </button>
+  );
 };
