51f3a6ed9feaff6e226b0c33090fa1e7be4a0de4
utin-francis-peter <u.francispeter@gmail.com>
2023-05-23 11:03:24 +0100

    refactor: cleaned up button stories to avoid args/props repeating across variants/states

--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -8,51 +8,33 @@ export default {
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary = {
-  args: {
-    label: "Building with SDN",
-    variant: "primary",
-    disabled: false,
-    small: false,
-    fullWidth: true,
-  },
+  args: {},
 };
 
 export const Secondary = {
   args: {
     label: "Secondary Button",
     variant: "secondary",
-    disabled: false,
-    small: false,
-    fullWidth: true,
   },
 };
 
 export const Disabled = {
   args: {
     label: "Disabled Button",
-    variant: "primary",
     disabled: true,
-    small: false,
-    fullWidth: true,
   },
 };
 
 export const Small = {
   args: {
     label: "Small Button",
-    variant: "primary",
-    disabled: false,
     small: true,
-    fullWidth: true,
   },
 };
 
 export const FullWidth = {
   args: {
     label: "Full Width Button",
-    variant: "primary",
-    disabled: false,
-    small: false,
     fullWidth: true,
   },
 };
