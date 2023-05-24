d8ae078934ebbde5c2f2a6b42e941719c37768c1
utin-francis-peter <u.francispeter@gmail.com>
2023-05-24 07:36:25 +0100

    style: added pointer cursor to accordion header

--- components/Accordion.jsx
+++ components/Accordion.jsx
@@ -2,14 +2,14 @@ export const Accordion = ({ title, body, showBody, onClick }) => {
   return (
     <div className={`border p-2 ${!showBody ? "hover:bg-red-400" : ""}`}>
       <header
-        className={`flex items-center justify-between rounded p-2 ${
+        className={`flex cursor-pointer items-center justify-between rounded p-2 text-lg ${
           showBody ? "border-2 border-red-500" : ""
         }`}
         onClick={onClick}>
         <h3>{title}</h3>
-        <h3>
+        <button>
           <i className={`fa-solid fa-chevron-${showBody ? "up" : "down"}`}></i>
-        </h3>
+        </button>
       </header>
       {showBody && <body className="my-5">{body}</body>}
     </div>
