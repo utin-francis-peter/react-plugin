1ac3e0f3ed0bccddfe611fb0492fd2fc104fa6a4
utin-francis-peter <u.francispeter@gmail.com>
2023-06-15 11:57:32 +0100

    feat: network dropdown component using dummy data

---
+++ stories/networkdropdown.stories.jsx
@@ -0,0 +1,18 @@
+import NetworkDropdown from "../components/NetworkDropdown";
+
+export default {
+  title: "Bungee/Components",
+  component: NetworkDropdown,
+};
+
+export const from = {
+  args: {
+    variant: "from",
+  },
+};
+
+export const to = {
+  args: {
+    variant: "to",
+  },
+};
