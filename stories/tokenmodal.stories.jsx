5c844605573dbbd13663dbbbeec9e6744fa9a545
utin-francis-peter <u.francispeter@gmail.com>
2023-07-03 10:16:58 +0100

    feat: token-modal comp init

---
+++ stories/tokenmodal.stories.jsx
@@ -0,0 +1,14 @@
+import TokenModal from "../components/token-modal";
+
+export default {
+  title: "Bungee/TokenModal",
+  component: TokenModal,
+};
+
+export const fromToken = {
+  args: {},
+};
+
+export const toToken = {
+  args: {},
+};
